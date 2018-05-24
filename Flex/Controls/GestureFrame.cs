7f11522c92dfa7bcc6403cb2d945365ab5527ab8
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2018-05-24 13:44:36 +0200

    Add minor explaination

--- Flex/Controls/GestureFrame.cs
+++ Flex/Controls/GestureFrame.cs
@@ -3,6 +3,10 @@ using Xamarin.Forms;
 
 namespace Flex.Controls
 {
+    /// <summary>
+    /// Override class to not apply the GestureFrameRenderer to every Frame 
+    /// class in the project, but just to the one that FlexButton uses
+    /// </summary>
     public class GestureFrame : Frame
     {
     }
